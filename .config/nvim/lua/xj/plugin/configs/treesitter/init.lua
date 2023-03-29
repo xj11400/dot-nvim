76b863d7f19df8dc7ac8124681a09b586f575cab
XJ Hsu <xj11400@gmail.com>
2023-03-30 04:27:54 +0800

    <feat>(plugin): treesitter

---
+++ .config/nvim/lua/xj/plugin/configs/treesitter/init.lua
@@ -0,0 +1,62 @@
+local conf = require('xj.core.config')
+
+return {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+        'windwp/nvim-ts-autotag',
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        'nvim-treesitter/nvim-treesitter-refactor'
+    },
+    event = 'BufEnter',
+    build = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter.configs').setup(vim.tbl_deep_extend('force', {
+            ensure_installed = {
+                'astro',
+                'c',
+                'cmake',
+                'cpp',
+                'css',
+                'go',
+                'html',
+                'javascript',
+                'jsdoc',
+                'json',
+                'lua',
+                'markdown',
+                'markdown_inline',
+                'php',
+                'python',
+                'regex',
+                'rust',
+                'scss',
+                'tsx',
+                'typescript',
+                'yaml'
+            },
+            highlight = {
+                enable = true,
+                use_languagetree = true
+            },
+            indent = {
+                enable = true
+            },
+            autotag = {
+                enable = true
+            },
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false
+            },
+            refactor = {
+                highlight_definitions = {
+                    enable = true
+                },
+                highlight_current_scope = {
+                    enable = false
+                }
+            }
+        }, conf.plugins.treesitter or {}))
+    end,
+    enabled = not vim.tbl_contains(conf.disable_builtin_plugins, 'treesitter')
+}
