6f16ef6a98cd6c2efc809fac838071c7e64d262a
XJ Hsu <xj11400@gmail.com>
2023-03-30 04:06:19 +0800

    <feat>(plugin): comments

---
+++ .config/nvim/lua/xj/plugin/configs/comments/init.lua
@@ -0,0 +1,25 @@
+local conf = require('xj.core.config')
+
+return {
+    'numToStr/Comment.nvim',
+    config = function()
+        require('Comment').setup(vim.tbl_deep_extend('force', {
+            pre_hook = function(ctx)
+                local U = require('Comment.utils')
+                local location = nil
+                if ctx.ctype == U.ctype.block then
+                    location = require('ts_context_commentstring.utils').get_cursor_location()
+                elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+                    location = require('ts_context_commentstring.utils').get_visual_start_location()
+                end
+
+                return require('ts_context_commentstring.internal').calculate_commentstring({
+                    key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
+                    location = location
+                })
+            end
+        }, conf.plugins.comment_nvim or {}))
+    end,
+    event = 'VeryLazy',
+    enabled = not vim.tbl_contains(conf.disable_builtin_plugins, 'comment-nvim')
+}
