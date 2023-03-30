965ab5a992c3db1740fb54667f717cf32c4dc573
XJ Hsu <xj11400@gmail.com>
2023-03-31 04:36:01 +0800

    <feat>(plugin): nui.nvim

--- .config/nvim/lua/xj/plugin/configs/_plugins/init.lua
+++ .config/nvim/lua/xj/plugin/configs/_plugins/init.lua
@@ -1,6 +1,7 @@
 local conf = require('xj.core.config')
 
 local plugins = vim.tbl_deep_extend('force', {
+    'MunifTanjim/nui.nvim',
     'jceb/vim-orgmode',
     'psliwka/vim-smoothie'
 }, conf.add_plugins)
