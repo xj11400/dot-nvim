28b9f4cf797efc43f6fc0a5381253b2ee828e137
XJ Hsu <xj11400@gmail.com>
2023-03-30 08:32:16 +0800

    <modify>(plugin): terminal

--- .config/nvim/lua/xj/plugin/configs/terminal/init.lua
+++ .config/nvim/lua/xj/plugin/configs/terminal/init.lua
@@ -14,14 +14,13 @@ return {
         }
     },
     config = function()
-        local g = vim.g
         local title = vim.env.SHELL
 
-        g.floaterm_width = 0.7
-        g.floaterm_height = 0.8
-        g.floaterm_title = '[' .. title .. ']:($1/$2)'
-        g.floaterm_borderchars = '─│─│╭╮╯╰'
-        g.floaterm_opener = 'vsplit'
+        vim.g.floaterm_width = 0.7
+        vim.g.floaterm_height = 0.8
+        vim.g.floaterm_title = '[' .. title .. ']:($1/$2)'
+        vim.g.floaterm_borderchars = '─│─│╭╮╯╰'
+        vim.g.floaterm_opener = 'vsplit'
 
         require('xj.plugin.configs.terminal.highlights')
         require('xj.plugin.configs.terminal.mappings')
