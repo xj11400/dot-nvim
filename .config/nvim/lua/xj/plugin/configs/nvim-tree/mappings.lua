d80cd3027f1415bc0a3082255ae87d8546170000
XJ Hsu <xj11400@gmail.com>
2023-03-30 08:32:01 +0800

    <modify>(plugin): nvim-tree

--- .config/nvim/lua/xj/plugin/configs/nvim-tree/mappings.lua
+++ .config/nvim/lua/xj/plugin/configs/nvim-tree/mappings.lua
@@ -1,5 +1,4 @@
 local map = require('xj.utility').map
 
-map('n', '<C-n>', ':NvimTreeToggle<CR>', { desc = 'Toggle Tree' })
 map('n', '<leader>nt', ':NvimTreeToggle<CR>', { desc = 'Toggle Tree' })
 map('n', '<leader>nr', ':NvimTreeRefresh<CR>', { desc = 'Refresh Tree' })
