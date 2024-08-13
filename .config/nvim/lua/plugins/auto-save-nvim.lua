449e3e17db479c931e0df29b901dbb870d44c3e0
XJ Hsu <xj11400@gmail.com>
2024-08-13 19:37:16 +0800

    (plugins/auto-save-nvim): auto-save.nivm

---
+++ .config/nvim/lua/plugins/auto-save-nvim.lua
@@ -0,0 +1,17 @@
+return {
+  "okuuva/auto-save.nvim",
+  cmd = "ASToggle", -- optional for lazy loading on command
+  event = { "InsertLeave", "TextChanged" },
+  opts = {
+    enabled = true,
+    -- trigger_events = { -- See :h events
+    --   immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+    --   defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+    --   cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+    -- },
+    debounce_delay = 2000,
+    execution_message = {
+      enabled = false,
+    },
+  },
+}
