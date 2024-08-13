83c461241bdd7f17da82a4c0e2a8ce1efc6fd731
XJ Hsu <xj11400@gmail.com>
2024-08-13 19:37:47 +0800

    (plugins/lsp_lines): lsp_lines.nvim

---
+++ .config/nvim/lua/plugins/lsp_lines.lua
@@ -0,0 +1,18 @@
+return {
+  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  config = function()
+    require("lsp_lines").setup()
+
+    -- Disable virtual_text since it's redundant due to lsp_lines.
+    vim.diagnostic.config {
+      virtual_text = false,
+      -- virtual_lines = {
+      --   only_current_line = true,
+      -- --   -- highlight_whole_line = false,.
+      -- },
+    }
+
+    -- hide in default
+    require("lsp_lines").toggle()
+  end,
+}
