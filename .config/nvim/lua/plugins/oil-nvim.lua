a7f05d1a48dd09f22522a78f33a89683be246b7f
XJ Hsu <xj11400@gmail.com>
2024-08-13 19:38:32 +0800

    (plugins/oil-nvim): oil.nvim

---
+++ .config/nvim/lua/plugins/oil-nvim.lua
@@ -0,0 +1,21 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {
+    keymaps = {
+      ["q"] = "actions.close",
+      ["<BS>"] = "actions.parent",
+    },
+    view_options = {
+      show_hidden = true,
+      -- This function defines what is considered a "hidden" file
+      is_hidden_file = function(name, bufnr) return vim.startswith(name, ".") end,
+      -- This function defines what will never be shown, even when `show_hidden` is set
+      is_always_hidden = function(name, bufnr) return vim.startswith(name, ".DS_Store") end,
+    },
+  },
+  enabled = true,
+  cmd = "Oil",
+  keys = {
+    { "<Localleader>O", function() require("oil").open_float() end, desc = "Explore with Oil" },
+  },
+}
