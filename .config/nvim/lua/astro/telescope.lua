62963d662e19d79987e8682534e934bd97d5ca1a
XJ Hsu <xj11400@gmail.com>
2024-08-17 03:02:35 +0800

    (astro/telescope): find hidden file, file ifnore patterns

--- .config/nvim/lua/astro/telescope.lua
+++ .config/nvim/lua/astro/telescope.lua
@@ -13,6 +13,18 @@ return {
             ["<C-J>"] = opts.defaults.mappings.n["<CR>"],
           },
         },
+        file_ignore_patterns = {
+          ".git/.*", -- Exclude the .git directory
+          "node_modules/.*", -- Example: Exclude node_modules
+          "yarn.lock", -- Exclude yarn.lock files
+          "package%-lock.json", -- Exclude package-lock.json (note the escape)
+          "lazy%-lock.json", -- Exclude lazy-lock.json (note the escape)
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
       },
     })
     return opts
