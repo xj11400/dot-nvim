5a52ce80d09b5f8ed25cd07ad3a656019e3e3751
XJ Hsu <xj11400@gmail.com>
2024-08-24 00:49:32 +0800

    (lazy_setup): add lsp

--- .config/nvim/lua/lazy_setup.lua
+++ .config/nvim/lua/lazy_setup.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
   { import = "community" },
   { import = "astro" },
   { import = "plugins" },
+  { import = "lsp" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "astrotheme", "habamax" } },
