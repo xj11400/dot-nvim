32b1a3e3e2b907629d6b7ed9d2516d93e4d8e68c
XJ Hsu <xj11400@gmail.com>
2024-08-24 00:41:38 +0800

    (lsp): nix

---
+++ .config/nvim/lua/lsp/nix.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "nix" })
+      end
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local builtins = require("null-ls").builtins
+      opts.sources = require("astrocore").list_insert_unique(opts.sources, {
+        builtins.code_actions.statix,
+        builtins.formatting.nixfmt,
+        builtins.diagnostics.deadnix,
+      })
+    end,
+  },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts) opts.servers = require("astrocore").list_insert_unique(opts.servers, { "nixd" }) end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        nix = { "nixfmt" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        nix = { "statix", "deadnix" },
+      },
+    },
+  },
+}
