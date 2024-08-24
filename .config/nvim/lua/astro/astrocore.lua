c466f5f5c9d40066a284310f5c62f6fafa97865c
XJ Hsu <xj11400@gmail.com>
2024-08-24 10:02:05 +0800

    (astro/astrocore): signcolumn = "yes:2"

--- .config/nvim/lua/astro/astrocore.lua
+++ .config/nvim/lua/astro/astrocore.lua
@@ -47,7 +47,7 @@ return {
         opt = { -- vim.opt.<key>
           clipboard = "", -- don't use "unnamedplus"
           cursorcolumn = true, -- Highlight the text line of the cursor column
-          signcolumn = "auto:3", -- Show both gitsign symbols and diagnostics symbols
+          signcolumn = "yes:2", -- Show both gitsign symbols and diagnostics symbols -- (auto:3)
         },
         g = { -- vim.g.<key>
           -- configure global vim variables (vim.g)
