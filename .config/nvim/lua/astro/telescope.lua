2f0aee71ccde9973543c723096d0c57fa8080921
XJ Hsu <xj11400@gmail.com>
2024-08-29 10:30:50 +0800

    (astro/telescope): configure ripgrep

--- .config/nvim/lua/astro/telescope.lua
+++ .config/nvim/lua/astro/telescope.lua
@@ -20,11 +20,45 @@ return {
           "package%-lock.json", -- Exclude package-lock.json (note the escape)
           "lazy%-lock.json", -- Exclude lazy-lock.json (note the escape)
         },
+        -- configure to use ripgrep
+        vimgrep_arguments = {
+          "rg",
+          "--follow", -- Follow symbolic links
+          "--hidden", -- Search for hidden files
+          "--no-heading", -- Don't group matches by each file
+          "--with-filename", -- Print the file path with the matched lines
+          "--line-number", -- Show line numbers
+          "--column", -- Show column numbers
+          "--smart-case", -- Smart case search
+
+          -- Exclude some patterns from search
+          -- "--glob=!**/.git/*",
+          -- "--glob=!**/.idea/*",
+          -- "--glob=!**/.vscode/*",
+          -- "--glob=!**/build/*",
+          -- "--glob=!**/dist/*",
+          -- "--glob=!**/yarn.lock",
+          -- "--glob=!**/package-lock.json",
+        },
       },
       pickers = {
         find_files = {
           hidden = true,
         },
+        -- needed to exclude some files & dirs from general search
+        -- when not included or specified in .gitignore
+        -- find_command = {
+        --   "rg",
+        --   "--files",
+        --   "--hidden",
+        --   "--glob=!**/.git/*",
+        --   "--glob=!**/.idea/*",
+        --   "--glob=!**/.vscode/*",
+        --   "--glob=!**/build/*",
+        --   "--glob=!**/dist/*",
+        --   "--glob=!**/yarn.lock",
+        --   "--glob=!**/package-lock.json",
+        -- },
       },
     })
     return opts
