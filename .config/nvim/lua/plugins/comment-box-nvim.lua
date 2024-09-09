882cbf6c93a34966f942bff9c1b490df8bfad96b
XJ Hsu <xj11400@gmail.com>
2024-09-09 10:06:49 +0800

    (plugins/comment-box): LudoPinelli/comment-box.nvim

---
+++ .config/nvim/lua/plugins/comment-box-nvim.lua
@@ -0,0 +1,44 @@
+return {
+  "LudoPinelli/comment-box.nvim",
+  -- opts = {
+  --   -- type of comments:
+  --   --   - "line":  comment-box will always use line style comments
+  --   --   - "block": comment-box will always use block style comments
+  --   --   - "auto":  comment-box will use block line style comments if
+  --   --              multiple lines are selected, line style comments
+  --   --              otherwise
+  --   comment_style = "auto",
+  --   doc_width = 80, -- width of the document
+  --   box_width = 60, -- width of the boxes
+  --   borders = { -- symbols used to draw a box
+  --     top = "─",
+  --     bottom = "─",
+  --     left = "│",
+  --     right = "│",
+  --     top_left = "╭",
+  --     top_right = "╮",
+  --     bottom_left = "╰",
+  --     bottom_right = "╯",
+  --   },
+  --   line_width = 70, -- width of the lines
+  --   lines = { -- symbols used to draw a line
+  --     line = "─",
+  --     line_start = "─",
+  --     line_end = "─",
+  --     title_left = "─",
+  --     title_right = "─",
+  --   },
+  --   outer_blank_lines_above = false, -- insert a blank line above the box
+  --   outer_blank_lines_below = false, -- insert a blank line below the box
+  --   inner_blank_lines = false, -- insert a blank line above and below the text
+  --   line_blank_line_above = false, -- insert a blank line above the line
+  --   line_blank_line_below = false, -- insert a blank line below the line
+  -- },
+  -- keys = {
+  --   {
+  --     "gcb",
+  --     mode = { "n", "v" },
+  --     desc = "Comment Box - Box",
+  --   },
+  -- },
+}
