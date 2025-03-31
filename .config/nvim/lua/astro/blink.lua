df96b5c1e96823e43cb319ff5725e70e9fff79c4
XJ Hsu <xj11400@gmail.com>
2025-03-31 08:18:38 +0800

    (astro/blink): blink

---
+++ .config/nvim/lua/astro/blink.lua
@@ -0,0 +1,10 @@
+return {
+  "Saghen/blink.cmp",
+  opts = {
+    keymap = {
+      ["<C-J>"] = { "accept", "fallback" },
+      ["<C-K>"] = {},
+      ["<C-e>"] = { "cancel", "fallback" },
+    }
+  },
+}
