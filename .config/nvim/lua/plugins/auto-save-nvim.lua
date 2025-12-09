6f6c3a526afbfe253fc0a622ba82b2b8da5ff9eb
XJ Hsu <xj11400@gmail.com>
2025-12-09 13:52:42 +0800

    (plugins/autosave): config condition

--- .config/nvim/lua/plugins/auto-save-nvim.lua
+++ .config/nvim/lua/plugins/auto-save-nvim.lua
@@ -1,3 +1,36 @@
+-- some recommended exclusions. you can use `:lua print(vim.bo.filetype)` to
+-- get the filetype string of the current buffer
+local excluded_filetypes = {
+  -- this one is especially useful if you use neovim as a commit message editor
+  "gitcommit",
+  -- most of these are usually set to non-modifiable, which prevents autosaving
+  -- by default, but it doesn't hurt to be extra safe.
+  "NvimTree",
+  "Outline",
+  "TelescopePrompt",
+  "alpha",
+  "dashboard",
+  "lazygit",
+  "neo-tree",
+  "oil",
+  "prompt",
+  "toggleterm",
+}
+
+local excluded_filenames = {
+  "do-not-autosave-me.lua",
+}
+
+local function save_condition(buf)
+  if
+    vim.tbl_contains(excluded_filetypes, vim.fn.getbufvar(buf, "&filetype"))
+    or vim.tbl_contains(excluded_filenames, vim.fn.expand "%:t")
+  then
+    return false
+  end
+  return true
+end
+
 return {
   "okuuva/auto-save.nvim",
   cmd = "ASToggle", -- optional for lazy loading on command
@@ -9,6 +42,7 @@ return {
     --   defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
     --   cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
     -- },
+    condition = save_condition,
     debounce_delay = 2000,
   },
 }
