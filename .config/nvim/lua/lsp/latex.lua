063e7128d980161bae3785db66c9fc718d11ae2c
XJ Hsu <xj11400@gmail.com>
2025-12-12 10:19:49 +0800

    (lsp/latex): latex

---
+++ .config/nvim/lua/lsp/latex.lua
@@ -0,0 +1,26 @@
+-- NOTE: nvim-treesitter latex require tree-sitter cli
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "latex" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ltex-ls-plus" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ltex-ls-plus" })
+    end,
+  },
+}
