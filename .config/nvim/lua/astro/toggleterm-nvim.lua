80cc46616f9f914bbb507b2612c052a68f257441
XJ Hsu <xj11400@gmail.com>
2025-12-12 16:44:06 +0800

    (astro/toggleterm): cmd

--- .config/nvim/lua/astro/toggleterm-nvim.lua
+++ .config/nvim/lua/astro/toggleterm-nvim.lua
@@ -5,16 +5,39 @@ return {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local maps = opts.mappings
-        local astro = require("astrocore")
+        local astro = require "astrocore"
 
-        -- ranger
-        if vim.fn.executable("ranger") == 1 then
-          maps.n["<Leader>tr"] = {
-            function() astro.toggle_term_cmd { cmd = "ranger", direction = "float" } end,
-            desc = "ToggleTerm Ranger",
-          }
+        function parseInput(input)
+          local pos, cmd
+
+          if input:match "^\\" then
+            if input:match "^\\h" or input:match "^\\v" then
+              local firstPart, remainder = input:match "^(%S+)%s*(.*)"
+              if firstPart == "\\h" then
+                pos = "horizontal"
+              elseif firstPart == "\\v" then
+                pos = "vertical"
+              end
+              cmd = remainder
+            else
+              error("Invalid command: input must start with '\\h' or '\\v', not '" .. input:match "^%S+" .. "'")
+            end
+          else
+            pos = "float"
+            cmd = input
+          end
+
+          return pos, cmd
         end
 
+        maps.n["<Leader>tc"] = {
+          function()
+            local input = vim.fn.input "[\\h\\v] cmd: "
+            local pos, cmd = parseInput(input)
+            astro.toggle_term_cmd { cmd = cmd, direction = pos }
+          end,
+          desc = "ToggleTerm cmd",
+        }
       end,
     },
   },
