a73e2ac812151a648be90c2fb5a53e1137fd33b8
XJ Hsu <xj11400@gmail.com>
2025-12-20 05:07:07 +0800

    (community): update community

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -10,22 +10,26 @@ return {
 
   -- packs
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.mdx" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
 
   -- import/override with your plugins folder
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.motion.mini-surround" },
-  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
-  { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.scrolling.mini-animate" },
   { import = "astrocommunity.register.nvim-neoclip-lua" },
   { import = "astrocommunity.terminal-integration.flatten-nvim" },
   { import = "astrocommunity.terminal-integration.toggleterm-manager-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
-
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.recipes.ai" },
   --
   -- { import = "astrocommunity." },
   { import = "astrocommunity.code-runner.overseer-nvim" },
@@ -34,6 +38,4 @@ return {
   --
   -- { import = "astrocommunity.note-taking.obsidian-nvim" },
   -- { import = "astrocommunity.note-taking.neorg" },
-
-  { import = "astrocommunity.recipes.ai" },
 }
