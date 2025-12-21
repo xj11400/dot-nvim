0da0a841af21ebda88ba554acdee241df9c2058d
XJ Hsu <xj11400@gmail.com>
2025-12-22 00:18:21 +0800

    (plugins/markdown): format

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,33 +1,35 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    -- highlights
-    local _zx = require "colors.zx"
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      -- highlights
+      local _zx = require "colors.zx"
 
-    vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { fg = _zx.ui.blue, bg = _zx.none })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { fg = _zx.ui.blue, bg = _zx.none })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { fg = _zx.ui.blue, bg = _zx.none })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { fg = _zx.ui.blue, bg = _zx.none })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { fg = _zx.ui.blue, bg = _zx.none })
-    vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { fg = _zx.ui.blue, bg = _zx.none })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { fg = _zx.ui.blue, bg = _zx.none })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { fg = _zx.ui.blue, bg = _zx.none })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { fg = _zx.ui.blue, bg = _zx.none })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { fg = _zx.ui.blue, bg = _zx.none })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { fg = _zx.ui.blue, bg = _zx.none })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { fg = _zx.ui.blue, bg = _zx.none })
 
-    require("render-markdown").setup {
-      heading = {
-        render_modes = true,
-        icons = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
-      },
-      bullet = {
-        enabled = false,
-      },
-      -- checkbox = {
-      --   enabled = false,
-      -- },
-      -- win_options = { concealcursor = { rendered = "nvc" } },
-      completions = {
-        blink = { enabled = true },
-        lsp = { enabled = true },
-      },
-    }
-  end,
+      require("render-markdown").setup {
+        heading = {
+          render_modes = true,
+          icons = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
+        },
+        bullet = {
+          enabled = false,
+        },
+        -- checkbox = {
+        --   enabled = false,
+        -- },
+        -- win_options = { concealcursor = { rendered = "nvc" } },
+        completions = {
+          blink = { enabled = true },
+          lsp = { enabled = true },
+        },
+      }
+    end,
+  },
 }
