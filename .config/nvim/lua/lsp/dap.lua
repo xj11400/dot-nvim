35a7f7a72ad9d4e71c50675b54bbd93361633889
XJ Hsu <xj11400@gmail.com>
2025-12-24 03:17:43 +0800

    (lsp/dap): configurations

--- .config/nvim/lua/lsp/dap.lua
+++ .config/nvim/lua/lsp/dap.lua
@@ -1,2 +1,38 @@
 return {
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      local dap = require "dap"
+
+      dap.adapters.codelldb = {
+        type = "executable",
+        command = "codelldb", -- or if not in $PATH: "/absolute/path/to/codelldb"
+      }
+
+      dap.configurations.cpp = {
+        {
+          name = "Launch file",
+          type = "codelldb",
+          request = "launch",
+          program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/build/", "file") end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+        },
+      }
+
+      dap.configurations.c = dap.configurations.cpp
+
+      dap.configurations.rust = {
+        {
+          name = "Launch file",
+          type = "codelldb",
+          request = "launch",
+          program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file") end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+        },
+      }
+      return opts
+    end,
+  },
 }
